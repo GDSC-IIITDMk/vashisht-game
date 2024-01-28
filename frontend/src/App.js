777c647d4be81bd54acb17512476ec143221829b
Pranav9347 <nadgirpranav624@gmail.com>
2024-01-28 19:29:05 +0530

    2nd update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState("a");
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  async function getUser() {
+    try {
+      const response = await axios.get('http://192.168.82.46:5000');
+      setData(response.data); // Use response.data to get the actual data
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {data}
         <a
           className="App-link"
           href="https://reactjs.org"
