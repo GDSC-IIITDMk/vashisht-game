70ddc91fb97a3c7141162a41f474a12075b51480
Pranav9347 <112823461+Pranav9347@users.noreply.github.com>
2024-02-03 15:45:59 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Leaderboard from '../../components/Leaderboard';
 import styles from "../Home/styles.module.css";
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
@@ -9,8 +10,13 @@ const Dashboard = (userDetails) => {
 
   return (
     <div>
-      Dashboard
+    <h2 style={{position: "relative",left: "40px",top: "30px"}}>Your Balance:  <div style={{position: "relative", left: "50px"}}>₹50</div></h2>
+    <div>
+      <Leaderboard />
     </div>
+    </div>
+    
+    
   );
 };
 
